6500f04537ce5701cc09798b48c01ea55f5f50f2
Narmin438 <bnermin2009@gmail.com>
2024-10-28 18:48:48 +0400

    js-2

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var min = 95
-var max = 100
-var randomNumber = Math.floor( Math.random() * (max-min + 1) + min ) 
-console.log(randomNumber)
+// var min = 95
+// var max = 100
+// var randomNumber = Math.floor( Math.random() * (max-min + 1) + min ) 
+// console.log(randomNumber)
+
+
+// num = 36;
+// num = num**5;
+// num = num%3;
+// console.log(num);
+
+firstName = "Narmin";
+lastName = "Babayeva";
+
+console.log("Sizin adiniz"+" "+ firstName + " " + lastName+ ".Tanisligimiza cox sadam!")
